b8570c1122f8a2336420137f82562b46c55dbcfe
Chris <c.chriv@gmail.com>
2014-01-27 16:58:00 +0100

    Queries soluce

--- 02.Queries/main.rb
+++ 02.Queries/main.rb
@@ -5,19 +5,32 @@ database_path = "db/jukebox.sqlite"
 db = SQLite3::Database.new(database_path)
 
 # 1. returns the list of tracks with their album and artist
-tracks = # your code here
+tracks = db.execute("SELECT Album.Title, 
+	Track.Name, 
+	Artist.Name
+	FROM Album 
+	INNER JOIN Track ON Track.AlbumId = Album.AlbumId 
+	INNER JOIN Artist ON Artist.ArtistId = Album.ArtistId")
 
+p tracks
 
 # 2. For each genre of music, finds the number of tracks and the average song length.
-# your code here.
+genres = db.execute("SELECT g.Name, count(t.Name)  
+	FROM Track t
+	INNER JOIN Genre g
+	ON t.GenreID=g.GenreID 
+	GROUP BY g.Name")
+
+p genres
 
 
 # 3. List the top 5 rock artists
 # your code here.
 
-# sql = " SELECT a.name, count(t.trackId) FROM Track t inner join Album b on t.albumId = b.albumId "
-# sql += " inner join Artist a on a.artistId = b.artistId "
-# sql += " inner join Genre g on g.genreId = t.genreId "
-# sql += " where g.name == 'Rock'  group by a.name order by count(t.trackId) DESC limit 5"
-# top_artists = db.execute(sql)
-# puts top_artists
+sql = " SELECT a.name, count(t.trackId) FROM Track t inner join Album b on t.albumId = b.albumId "
+sql += " inner join Artist a on a.artistId = b.artistId "
+sql += " inner join Genre g on g.genreId = t.genreId "
+sql += " where g.name == 'Rock'  group by a.name order by count(t.trackId) DESC limit 5"
+top_artists = db.execute(sql)
+puts top_artists
+
